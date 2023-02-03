85b6ed14764df8fd64e9f17b6c75de8b07191526
mstr18 <mstr18@gmail.com>
2023-02-03 07:49:45 -0300

    Validação de senha e cript em construção

--- Classes/conexoes.py
+++ Classes/conexoes.py
@@ -1,4 +1,6 @@
 import pyodbc
+import bcrypt
+import re
 
 class Conexoes:
     
@@ -6,16 +8,36 @@ class Conexoes:
     def __init__(self):
         try:
             self.conn = pyodbc.connect("DRIVER={SQL Server};SERVER=localhost;PORT=1433;DATABASE=master;UID=sa;PWD=Abc,1234")
-        except pyodbc.Error:
-            return print("Erro na conexao com o banco de dados")
+        except pyodbc.Error as err:
+            return print(f"Erro na conexao com o banco de dados: {err}")
         
     def getConn(self):
+
         return self.conn
+    
+    def insertUser(u,p,e, self)
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+        if(re.search(regex,e)):  
+                    
+                    password = p.encode() 
+                    salt = bcrypt.gensalt()
+                    password_hash = bcrypt.hashpw(password, salt)
+                    #Validar senha!!!! terminar código
+                    self.conn.cursor().execute(f"insert into dbo.users values ('{u}', '{password_hash}', '{e}');")
+                    self.conn.commit() 
+                    return True
+        else:  
+                    print("Invalid Email")  
+                    return False
     
     def validUser(self, u, p):
         cursor = self.conn.cursor().execute(f"select usuario, senha from dbo.users where usuario like '{u}' and senha like '{p}'")
-        if cursor.rowcount == 0: return False
-        else: return True
+        if cursor.rowcount == 0: 
+            return False
+        else: 
+            cursor.close()
+            return True
         
     
 
--- primeiro_site.py
+++ primeiro_site.py
@@ -1,20 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import timedelta
 from Classes.conexoes import Conexoes
+import re
 
+#import ipdb import para depuração
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-bd = Conexoes()
 
-# Dummy user database
-#users = {
-#    'user1': 'p1',sudo pip install 
-#   'user2': 'p2',
-#    'user3': 'p3'
-#}
+#Inicializando classe de validação de usuários. Senhas ainda em plaintext
+bd = Conexoes()
 
 @app.route("/")
 def home():
@@ -30,6 +27,7 @@ def login():
         # Get the username and password from the form
         username = request.form["username"]
         password = request.form["password"]
+        
 
         # Check if the user exists and the password is correct
         if bd.validUser(username, password):
@@ -45,28 +43,43 @@ def index():
      if 'username' in session:
          return render_template("index.html")
      else:
-         return "sesssão expirada <a href='/login'> Voltar </a>"
+         return render_template("login.html",invalido = "Sessão Expirada, favor logar novamente*" )
      
 @app.route("/generic")
 def generic():
      if 'username' in session:
          return render_template("generic.html")
      else:
-         return "sesssão expirada <a href='/login'> Voltar </a>"
+         return render_template("login.html",invalido = "Sessão Expirada, favor logar novamente*" )
         
 @app.route("/elements")
 def elements():
      if 'username' in session:
          return render_template("elements.html")
      else:
-         return "sesssão expirada <a href='/login'> Voltar </a>"
+         return render_template("login.html",invalido = "Sessão Expirada, favor logar novamente*" )
 
 @app.route("/logout")
 def logout():
     session.pop('username', None)
     return redirect(url_for("home"))
 
+@app.route("/register", methods=["POST", "GET"])
+def register():
 
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    #if(re.search(regex,email)):  validação, se True, email OK
+         
+    if request.method == "POST":
+            # Get the username and password from the form
+            username = request.form["username"]
+            password = request.form["password"]
+            passwordconfirm = request.form["passwordconfirm"]
+            email = request.form["email"]
+
+     #adicionar regras#
+
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
